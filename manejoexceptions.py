99cfbbea2f12cb3fd466f7f6e9b1aa6f9a5bcc73
audio3544 <juaninsis@gmail.com>
2017-03-15 21:15:27 -0300

    se agregan exceptions

---
+++ manejoexceptions.py
@@ -0,0 +1,26 @@
+class UnoError(Exception):
+	def __init__(self,valor):
+		self.valorError=valor
+	def __str__(self):
+		return ("No puede dividir por 1 a "+str(self.valorError))
+
+print "Hola mundo"
+d=5
+n=1
+try:
+	if n == 1:
+		raise UnoError(5)
+	print d/n
+except (TypeError,NameError):
+	print "error en tipo de dato" 
+except ZeroDivisionError:
+	print "no puede dividir por cero"
+except UnoError as msg:
+	print msg
+else: #se ejecuta el else si no se ejecuto con error
+	print "no hubo error"
+finally:
+	print "siempre se ejecuta","usar para cerrar conexiones por ejemplo"
+
+
+print "Chau"
